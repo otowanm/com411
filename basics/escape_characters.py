9b5ff3362348dd7a8d607d87135feca80c3f38ee
Michael Otowan <6otowm03@solent.ac.uk>
2022-11-02 13:53:47 +0000

    Tested

---
+++ basics/escape_characters.py
@@ -0,0 +1,14 @@
+# Display escape characters
+#Add the following print commands.
+
+# print("\n Displays a new line")
+# print("\t Displays a tab space")
+# print("\\ Displays a back slash")
+# print("\" Displays a double quote")
+# print("\' Displays a single quote")
+
+print("\n Displays a new line")
+print("\t Displays a tab space")
+print("\\ Displays a back slash")
+print("\" Displays a double quote")
+print("\' Displays a single quote")
